b3a74ba1b83bb24bd37605a1a592decbe0ea46a1
LINOYKR <linoykr@gmail.com>
2026-04-14 20:29:57 +0300

    changed to uppercase

--- linoy.java
+++ linoy.java
@@ -1,6 +1,6 @@
 public class Linoy {
     public static void main(String[] args) {
-        for(int i=1; i<=5; i++) {
+        for (int i=1; i<=5; i++) {
             System.out.println("linoy..." + i);
         }
     }
